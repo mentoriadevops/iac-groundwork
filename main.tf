715f39482fb8debe7bd62d18d450c5ebc635fe27
kadeguilherme <kadeguilherme@gmail.com>
2022-09-16 23:11:32 -0300

    feat: Refatoracao do groundwork
    Signed-off-by: kadeguilherme <kadeguilherme@gmail.com>
    Co-authored-by: Luiz Aoqui <lgfa29@gmail.com>
    Co-authored-by: Danilo F Rocha <snifbr@gmail.com>
    Co-authored-by: Marcelo Paglione <marcelo@link-info.com>

--- main.tf
+++ main.tf
@@ -1,51 +1,24 @@
 provider "google" {
-  project = var.project
-  region  = var.region
+  project = "mentoriaiac-20220916"
+  region  = "us-central1"
 }
 
-module "groundwork" {
-  source   = "github.com/mentoriaiac/iac-modulo-groundwork.git?ref=v0.1.0"
-  project  = var.project
-  vpc_name = "groundwork"
-  subnetworks = [
-    {
-      name          = "rede-us-central1"
-      ip_cidr_range = "10.0.0.0/16"
-      region        = "us-central1"
-    },
-    {
-      name          = "rede-us-west1"
-      ip_cidr_range = "10.10.0.0/16"
-      region        = "us-west1"
-    }
-  ]
-
-  firewall_allow = [
-    {
-      protocol = "tcp"
-      port     = [443, 80]
-    }
-  ]
+resource "google_compute_network" "vpc" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
 }
 
-variable "project" {
-  description = "Nome do projeto (Default é staging)"
-  type        = string
-  default     = "direct-link-325016"
+resource "google_compute_subnetwork" "load-balance" {
+  name          = "load-balance"
+  ip_cidr_range = "10.2.0.0/16"
+  network       = google_compute_network.vpc.id
+
 }
 
-variable "region" {
-  description = "Nome da região"
-  type        = string
-  default     = "us-central1"
+resource "google_compute_subnetwork" "nomad" {
+  name          = "nomad"
+  ip_cidr_range = "10.1.0.0/16"
+  network       = google_compute_network.vpc.id
+
 }
 
-output "vpc_id" {
-  description = "Retorna o id da VPC criada"
-  value       = module.groundwork.vpc_id
-}
-
-output "subnets_id" {
-  description = "Retorna uma lista de objetos com os atributos das subnets criadas"
-  value       = module.groundwork.subnets_id
-}
